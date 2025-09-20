3ad4d6be5ef50a6d51e158258fd05942adaecdc8
LisaJowett <lisajowett@live.ca>
2025-09-19 18:33:58 -0600

    Create Note added

--- router.php
+++ router.php
@@ -1,14 +1,6 @@
 <?php
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path']; //current uri
-
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/notes' => 'controllers/notes.php',
-    '/note' => 'controllers/note.php',
-    '/contact' => 'controllers/contact.php',
-];
+$routes = require('routes.php');
 
 function routeToController($uri, $routes) {
 
@@ -30,5 +22,7 @@ function abort($code = 404) {
 
     die();
 }
+
+$uri = parse_url($_SERVER['REQUEST_URI'])['path']; //current uri
 
 routeToController($uri, $routes);
